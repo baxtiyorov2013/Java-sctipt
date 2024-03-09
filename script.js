f88ea789dcc184791ded58251c00b0fbaaa76c4c
baxtiyorov2013 <baxtiyorov2013>
2024-03-09 11:21:32 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let ism = prompt("Ismingizni kriting");
+let familya = prompt("Familiyangiz kriting");
+let yosh = prompt("Yoshingizni kriting");
+let fan= prompt("Yoqtirgan faningizni kriting");
+let tuman =prompt("Yashaydigan tumaningizni kiriing")
+alert ("Malumotlarni tog'ri kiritganingiz uchun tashakkur")
+
+console.log("Ism: " + ism);
+console.log("Famliya:" + familya );
+console.log("Yosh:" + yosh);
+console.log("Fan:" + fan);
+console.log("Tuman:" + tuman);
